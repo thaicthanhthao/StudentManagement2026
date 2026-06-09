8d58660eb704c3a75632b64b053da42b8bbef62b
Levinhtoan <levinhtoan2004@gmail.com>
2026-06-09 22:01:58 +0700

    feat: add user update and soft delete api

--- src/main/java/student/techzen/controller/UserController.java
+++ src/main/java/student/techzen/controller/UserController.java
@@ -1,12 +1,43 @@
 package student.techzen.controller;
 
-import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import student.techzen.dto.ApiResponse;
+import student.techzen.dto.user.UserDetailResponse;
+import student.techzen.dto.user.UserUpdateRequest;
+import student.techzen.service.UserService;
+
+import java.util.UUID;
 
 @RestController
+@RequestMapping("${api.prefix}/users")
 @RequiredArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
 public class UserController {
+    private final UserService userService;
+
+    @PutMapping("/{id}")
+
+    public ResponseEntity<ApiResponse<UserDetailResponse>> updateUser(@PathVariable UUID id, @RequestBody UserUpdateRequest request) {
+
+        return ResponseEntity.ok(ApiResponse.<UserDetailResponse>builder()
+                .success(true)
+                .data(userService.updateUser(id, request))
+                .build()
+        );
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<String>> deleteUser(@PathVariable UUID id){
+
+        userService.deleteUser(id);
+
+        return ResponseEntity.ok(ApiResponse.<String>builder()
+                        .success(true)
+                        .data("User deactivated successfully")
+                        .build()
+        );
+    }
+
 }
--- src/main/java/student/techzen/service/UserServiceImpl.java
+++ src/main/java/student/techzen/service/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package student.techzen.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import student.techzen.dto.user.UserDetailResponse;
 import student.techzen.dto.user.UserUpdateRequest;
+import student.techzen.entity.User;
 import student.techzen.mapper.UserMapper;
 import student.techzen.repository.UserRepository;
 
@@ -19,11 +23,51 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDetailResponse updateUser(UUID id, UserUpdateRequest request) {
-        return null;
+
+        User user = userRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        if (request.getUsername() != null) {
+
+            if (request.getUsername().equals(user.getUsername()) ) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username already exists");
+            }
+
+            user.setUsername(request.getUsername());
+        }
+
+        if(request.getEmail() != null){
+
+            if(!request.getEmail().equals(user.getEmail()) && userRepo.existsByEmail(request.getEmail())){
+
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email already exists");
+            }
+
+            user.setEmail(request.getEmail());
+        }
+
+        if(request.getRole() != null){
+
+            user.setRole(request.getRole());
+        }
+
+        user = userRepo.save(user);
+
+        return userMapper.toDetailResponse(user);
     }
 
     @Override
     public void deleteUser(UUID id) {
 
+        User user = userRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        if("INACTIVE".equals(user.getStatus())){
+
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User already inactive");
+
+        }
+
+        user.setStatus("INACTIVE");
+
+        userRepo.save(user);
     }
 }
